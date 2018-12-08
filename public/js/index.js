0dd265c924e71e229bf3c317b926fa89e745b957
Ronny Moura <ronimour@gmail.com>
2018-12-08 11:37:13 -0200

    Add new user joined and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function(){
   console.log('Connected to server');
-  socket.emit('createMessage', {
-    to: 'userTwo@example',
-    text: 'Hello jeez'
-  });
-
 });
 
 socket.on('disconnect', function(){
--- server/server.js
+++ server/server.js
@@ -15,17 +15,33 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.emit('newMessage', {
-    from: 'userOne@example.com',
-      text: 'Meet me at 6pm',
-        createAt: new Date()
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createAt: new Date()
   });
 
-  socket.on('createMessage', (message) => {
-    console.log(message);
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: `New user ${socket.id} joined`,
+    createAt: new Date()
   });
+  // socket.emit('newMessage', {
+  //   from: 'userOne@example.com',
+  //   text: 'Meet me at 6pm',
+  //   createAt: new Date()
+  // });
+
+  // socket.on('createMessage', (message) => {
+  //   console.log('createMessage', message);
+  //   io.emit('newMessage', {
+  //     from: message.from,
+  //     text: message.text,
+  //     createAt: new Date()
+  //   });
+  // });
 
   socket.on('disconnect', () => {
-    console.log('User was disconnected');
+    console.log(`User ${socket.id} was disconnected`);
   });
 });
 
